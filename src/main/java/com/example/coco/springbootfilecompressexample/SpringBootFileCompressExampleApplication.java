d3011b3d5e8ace2a853edacca1cd07ebdc404015
CocoBhaiya <utsavbhaiya7@gmail.com>
2024-04-06 12:18:32 +0530

    file compressed and password protacted

---
+++ src/main/java/com/example/coco/springbootfilecompressexample/SpringBootFileCompressExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.coco.springbootfilecompressexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileCompressExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileCompressExampleApplication.class, args);
+	}
+
+}
